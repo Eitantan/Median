86bb5e1be067ab925e190a5c18f7cf5dfcaa7d0c
Eitantan <eitan.alperstein@gmail.com>
2024-01-17 07:39:10 -0700

    vercel

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-/* 
-For the encrypt/decrypt function:
-
-MIT License
-
-Copyright (c) 2018 CrypTools
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 const randomstring = require("randomstring");
 const express = require("express");
 const bodyParser = require("body-parser");
 const key = process.env["SECRET_KEY"];
-const Database = require("replpersist");
-const users = new Database("users");
+const users = {}
+users.data = {}
 const EIGHT_HOURS = 280000000;
 const appFiller = require("./app.filler.js")
 const e = require("./encrypt.js")
@@ -71,7 +46,6 @@ app.post("/signup.html", (req, res) => {
     }
   } else if (req.body.formtype === "login") {
     if (users.data[req.body.username] == undefined) {
-      console.log(users.data);
       res.redirect("/signup.html?err=1");
     } else if (
       users.data[req.body.username].password !== req.body.password.encrypt(key)
